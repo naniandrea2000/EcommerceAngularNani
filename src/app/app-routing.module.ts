b68a1cc87456df38809f03369bcb668c8d4053c9
naniandrea2000 <andreanani1400@gmail.com>
2020-09-08 12:11:11 +0200

    Inzio css header

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,6 +9,7 @@ const routes: Routes = [
   {path: '', redirectTo: '/login', pathMatch: 'full'},
   { path: 'login', loadChildren: () => import('./features/login/login.module').then(m => m.LoginModule) },
   { path: 'signin', loadChildren: () => import('./features/register/register.module').then(m => m.RegisterModule) },
+  { path: 'home', loadChildren: () => import('./features/home/home.module').then(m => m.HomeModule) },
 ]
 
 @NgModule({
