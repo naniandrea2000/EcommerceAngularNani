13796bc4946cd26bfac8bb5cdc331415e57de99a
naniandrea2000 <andreanani1400@gmail.com>
2020-09-08 11:26:00 +0200

    Aggiunta icone e creazione css login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,21 @@
+
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [{ path: 'Login', loadChildren: () => import('./features/login/login.module').then(m => m.LoginModule) }, { path: 'Home', loadChildren: () => import('./features/home/home.module').then(m => m.HomeModule) }, { path: 'Register', loadChildren: () => import('./features/register/register.module').then(m => m.RegisterModule) }, { path: 'Personalizza', loadChildren: () => import('./features/personalizza/personalizza.module').then(m => m.PersonalizzaModule) }, { path: 'Carrello', loadChildren: () => import('./features/carrello/carrello.module').then(m => m.CarrelloModule) }];
+
+
+const routes: Routes = [
+  {path: '', redirectTo: '/login', pathMatch: 'full'},
+  { path: 'login', loadChildren: () => import('./features/login/login.module').then(m => m.LoginModule) },
+]
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [
+      RouterModule.forRoot(routes),
+  ],
+  exports: [
+      RouterModule
+  ]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule{ }
