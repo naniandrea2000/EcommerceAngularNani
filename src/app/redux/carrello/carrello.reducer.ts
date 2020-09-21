f5671142dda6e2113a6e14833da838c82d4e2363
naniandrea2000 <andreanani1400@gmail.com>
2020-09-21 15:35:10 +0200

    Creazione carrello redux

---
+++ src/app/redux/carrello/carrello.reducer.ts
@@ -0,0 +1,24 @@
+import { createReducer, on, Action } from '@ngrx/store'
+import { User } from 'src/app/core/model/user.interface';
+import { saveCurrentUser, initUsers, insertUser } from './carrello.action';
+
+export interface LoginRegisterState {
+    currentUser: User;
+    users: User[];
+}
+
+export const initialState: LoginRegisterState = {
+    currentUser: null,
+    users: []
+};
+
+const loginRegisterReducerFun = createReducer(
+    initialState,
+    on(saveCurrentUser, (state, { user }) => ({ ...state, currentUser: user })),
+    on(initUsers, (state, { users }) => ({ ...state, users: users })),
+    on(insertUser, (state, { user }) => ({ ...state, users: [...state.users, user] }))
+);
+
+export function loginRegisterReducer(state: LoginRegisterState | undefined, action: Action) {
+    return loginRegisterReducerFun(state, action);
+}
---
+++ src/app/redux/carrello/index.ts
@@ -0,0 +1,10 @@
+ import { LoginRegisterState } from './carrello.reducer';
+import { createSelector } from '@ngrx/store';
+import { AppState } from '..';
+
+export const selectloginRegisterState = (state: AppState) => state.LoginRegisterState;
+
+export const getCurrentUser = createSelector(
+    selectloginRegisterState,
+    (state: LoginRegisterState) => state.currentUser
+);
